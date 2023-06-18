4d4231c262e6106fd5b17826c10395d7377a704d
josie9803 <josietran.03@gmail.com>
2023-06-19 00:40:56 +0700

    finish layout 1,4

--- app/src/main/java/com/example/numbermemorygame/MainActivity1.kt
+++ app/src/main/java/com/example/numbermemorygame/MainActivity1.kt
@@ -1,11 +1,23 @@
 package com.example.numbermemorygame
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.widget.Button
 
 class MainActivity1 : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main1)
+
+        val actButton = findViewById<Button>(R.id.startbutton)
+        actButton.setOnClickListener {
+            val Intent = Intent(this, MainActivity2::class.java)
+            startActivity(Intent)
+            finish()
+        }
     }
 }
