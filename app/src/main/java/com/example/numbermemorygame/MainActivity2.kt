54a5daa8fa39ea5db540e34e12bb2edc2b5d649c
deedeeNg <hvn5@sfu.ca>
2023-06-18 10:14:19 -0700

    Finishing activity2

--- app/src/main/java/com/example/numbermemorygame/MainActivity2.kt
+++ app/src/main/java/com/example/numbermemorygame/MainActivity2.kt
@@ -1,11 +1,49 @@
 package com.example.numbermemorygame
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity2 : AppCompatActivity() {
+
+    private val handler: Handler = Handler()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        var level = 10
+        var answer = ""
+        var progressStatus = 0
+
+        for (i in 1..level){
+            answer += (0..9).random().toString()
+        }
+
+        val answerText : TextView = findViewById<TextView>(R.id.answer)
+
+        answerText.setText(answer)
+
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+
+        Thread {
+            while (progressStatus < 100) {
+                progressStatus += 1
+                // Update the progress bar and display the
+                //current value in the text view
+                handler.post(Runnable {
+                    progressBar.progress = progressStatus
+                })
+                try {
+                    // Sleep for 200 milliseconds.
+                    Thread.sleep(100)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+            }
+        }.start()
+
     }
 }
